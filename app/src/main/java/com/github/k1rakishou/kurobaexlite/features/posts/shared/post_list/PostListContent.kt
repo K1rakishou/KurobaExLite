e7cafad61380e69e466f24cbc992724be8588aa9
k1rakishou <kez1911@protonmail.com>
2022-05-31 17:31:07 +0300

    Fix processPostListScrollEvent() receiving outdated postListAsync state which breaks stuff like last scrolled post position, bookmarks unread posts counter, etc.

--- app/src/main/java/com/github/k1rakishou/kurobaexlite/features/posts/shared/post_list/PostListContent.kt
+++ app/src/main/java/com/github/k1rakishou/kurobaexlite/features/posts/shared/post_list/PostListContent.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -114,13 +115,14 @@ internal fun PostListContent(
   val openedFromScreenKey = postListOptions.ownerScreenKey
   val postListAsyncMut by postsScreenViewModel.postScreenState.postsAsyncDataState.collectAsState()
   val postListAsync = postListAsyncMut
+  val postListAsyncUpdated by rememberUpdatedState(newValue = postListAsync)
 
   val lazyListState = rememberLazyListState()
 
   fun processPostListScrollEventFunc() {
     processPostListScrollEvent(
       postsScreenViewModel = postsScreenViewModel,
-      postListAsync = postListAsync,
+      postListAsync = postListAsyncUpdated,
       lazyListState = lazyListState,
       chanDescriptor = chanDescriptor,
       orientation = postListOptions.orientation,
