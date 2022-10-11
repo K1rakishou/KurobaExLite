3fa0bc0b44d60f5857d039db3a45e24248e3ec7e
k1rakishou <kez1911@protonmail.com>
2022-10-11 20:55:19 +0600

    Fix Modifier.verticalScrollbar() (which is used with ScrollState) not drawing the scrollbar.

--- app/src/main/java/com/github/k1rakishou/kurobaexlite/ui/helpers/modifier/ScrollbarModifiers.kt
+++ app/src/main/java/com/github/k1rakishou/kurobaexlite/ui/helpers/modifier/ScrollbarModifiers.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Stable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
@@ -298,7 +299,7 @@ fun Modifier.scrollbar(
 }
 
 /**
- * scrollbar for LazyLists that use ScrollState (like verticalScroll())
+ * scrollbar for LazyLists
  * */
 fun <ItemInfo : LazyItemInfoWrapper, LayoutInfo : LazyLayoutInfoWrapper<ItemInfo>> Modifier.scrollbar(
   lazyStateWrapper: LazyStateWrapper<ItemInfo, LayoutInfo>,
@@ -553,8 +554,9 @@ fun Modifier.verticalScrollbar(
     val scrollbarHeight = with(density) { 16.dp.toPx() }
     val thumbColor = chanTheme.scrollbarThumbColorDragged
 
-    val currentValue by remember { derivedStateOf { scrollState.value } }
-    val maxValue by remember { derivedStateOf {  scrollState.maxValue } }
+    val scrollStateUpdated by rememberUpdatedState(newValue = scrollState)
+    val currentPositionPx by remember { derivedStateOf { scrollStateUpdated.value } }
+    val maxScrollPositionPx by remember { derivedStateOf { scrollStateUpdated.maxValue } }
 
     val topPaddingPx = with(density) {
       remember(key1 = contentPadding) { contentPadding.calculateTopPadding().toPx() }
@@ -563,9 +565,9 @@ fun Modifier.verticalScrollbar(
       remember(key1 = contentPadding) { contentPadding.calculateBottomPadding().toPx() }
     }
 
-    val duration = if (scrollState.isScrollInProgress) 150 else 1000
-    val delay = if (scrollState.isScrollInProgress) 0 else 1000
-    val targetThumbAlpha = if (scrollState.isScrollInProgress) 0.8f else 0f
+    val duration = if (scrollStateUpdated.isScrollInProgress) 150 else 1000
+    val delay = if (scrollStateUpdated.isScrollInProgress) 0 else 1000
+    val targetThumbAlpha = if (scrollStateUpdated.isScrollInProgress) 0.8f else 0f
 
     val thumbAlphaAnimated by animateFloatAsState(
       targetValue = targetThumbAlpha,
@@ -578,17 +580,13 @@ fun Modifier.verticalScrollbar(
     return@composed Modifier.drawWithContent {
       drawContent()
 
-      if (maxValue == Int.MAX_VALUE || maxValue == 0) {
+      if (maxScrollPositionPx == Int.MAX_VALUE || maxScrollPositionPx == 0) {
         return@drawWithContent
       }
 
       val availableHeight = this.size.height - scrollbarHeight - topPaddingPx - bottomPaddingPx
-      if (availableHeight > maxValue) {
-        return@drawWithContent
-      }
-
-      val unit = availableHeight / maxValue.toFloat()
-      val scrollPosition = currentValue * unit
+      val unit = availableHeight / maxScrollPositionPx.toFloat()
+      val scrollPosition = currentPositionPx * unit
 
       val offsetX = this.size.width - scrollbarWidth
       val offsetY = topPaddingPx + scrollPosition
